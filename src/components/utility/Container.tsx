515df7dadf3fbfc56cf7bd5894762d3b30d3d0ee
Xancia <cminhma@gmail.com>
2023-12-18 20:00:58 -0500

    removed unneed stuff, added container component

---
+++ src/components/utility/Container.tsx
@@ -0,0 +1,17 @@
+interface ContainerProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Container: React.FC<ContainerProps> = ({
+    children,
+    className
+}) => {
+    return (
+        <div className={`max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ${className || ""}`}>
+            {children}
+        </div>
+    );
+};
+
+export default Container;
