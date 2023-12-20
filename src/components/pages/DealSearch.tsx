c9ae2095c76a61d831832a4d4455ce5a8a32f97a
Xancia <cminhma@gmail.com>
2023-12-20 03:29:19 -0500

    search bar works now

--- src/components/pages/DealSearch.tsx
+++ src/components/pages/DealSearch.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import GameCard from "../GameCard";
 import { NavBar } from "../NavBar";
 import { Input } from "../ui/input";
@@ -6,13 +7,15 @@ import { Button } from "../ui/button";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { setdealData } from "../utility/dealDataSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { DealType } from "@/vite-env";
 import { RootState } from "../utility/store";
 
 const DealSearch = () => {
   const dealData = useSelector((state: RootState) => state.dealData as DealType[] | null);
   const dispatch = useDispatch();
+  const [searching, setSearching] = useState(false)
+  const [input, setinput] = useState("")
 
   const url =
     "https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=0&steamRating=0&desc=0&output=json&steamworks=0&sortBy=Deal%20Rating&AAA=0&pageSize=20&exact=0&upperPrice=50&pageNumber=0&onSale=true&metacritic=70&storeID%5B0%5D=1%2C2%2C3";
@@ -38,6 +41,33 @@ const DealSearch = () => {
   useEffect(() => {
     fetchData()
   }, [])
+
+  const url2 = `https://cheapshark-game-deals.p.rapidapi.com/deals?lowerPrice=0&steamRating=0&title=${input}&desc=0&output=json&steamworks=0&sortBy=Deal%20Rating&AAA=0&pageSize=20&exact=0&upperPrice=50&pageNumber=0&onSale=true&storeID%5B0%5D=1%2C2%2C3`;
+  const options2 = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6adda4dea7msh0d84ee148b8b875p14ba25jsn3fd070b06a7c',
+		'X-RapidAPI-Host': 'cheapshark-game-deals.p.rapidapi.com'
+	}
+};
+
+    async function fetchSearched() {
+        try {
+            const response = await fetch(url2, options2);
+            const result = await response.json();
+            console.log(result);
+            dispatch(setdealData(result))
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    function handleSubmit() {
+        setSearching(true)
+        fetchSearched()
+    }
   
 
   return (
@@ -45,12 +75,12 @@ const DealSearch = () => {
       <NavBar />
       <Container className="pt-28">
         <div className="flex">
-          <Input type="text" placeholder="Search for a specific game" />
-          <Button type="submit">Search</Button>
+          <Input type="text" placeholder="Search for a specific game" value={input} onChange={(e) => setinput(e.target.value)}/>
+          <Button type="submit" onClick={handleSubmit}>Search</Button>
         </div>
 
         <div className="mt-10">
-          <p className="text-center font-bold text-3xl p-4">Check Out These Hot Deals!</p>
+          {searching ? <p className="text-center font-bold text-3xl p-4 capitalize">Here are the Best Deals for: {input}</p> : <p className="text-center font-bold text-3xl p-4">Check Out These Hot Deals!</p>}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 sm:gap-10">
             {dealData && dealData.map((deal:DealType) => (
                 <GameCard key={deal.dealID} deal={deal}/>
