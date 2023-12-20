82685bf62f371242f8e11fa8a9df7121c73cb7f8
Xancia <cminhma@gmail.com>
2023-12-20 01:07:51 -0500

    styling

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -3,7 +3,7 @@ import Container from "../utility/Container"
 
 const Contact = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 via-gray-50 to-gray-200 dark:from-slate-950 dark:via-slate-950 dark:to-slate-800">
         <NavBar />
         <Container className="pt-28">
                 Contact
