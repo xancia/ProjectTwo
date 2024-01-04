30f936510b4b4ac250062b222a19b7ea3e340781
Xancia <cminhma@gmail.com>
2024-01-04 12:03:28 -0500

    added a filter to search for exact matches

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -6,7 +6,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card";
 
 type ReviewCardProps = {
   reviews: ReviewType[];
@@ -15,27 +15,54 @@ type ReviewCardProps = {
 const ReviewCard: React.FC<ReviewCardProps> = ({ reviews }) => {
   return (
     <Carousel
-    opts={{
-      align: "start",
-    }}
-    className="w-10/12"
-  >
-    <CarouselContent>
-      {reviews.map((review, index) => (
-        <CarouselItem key={index} className="flex items-center md:basis-1/2 lg:basis-1/3">
-          <div className="p-1">
-            <Card className="">
-              <CardContent className="flex aspect-square items-center justify-center p-6">
-                <span className="text-sm font-semibold">{review.snippet}</span>
-              </CardContent>
-            </Card>
-          </div>
-        </CarouselItem>
-      ))}
-    </CarouselContent>
-    <CarouselPrevious />
-    <CarouselNext />
-  </Carousel>
+      opts={{
+        align: "start",
+      }}
+      className="w-9/12 md:w-10/12 lg:w-full"
+    >
+      <CarouselContent>
+        {reviews.map((review, index) => (
+          <CarouselItem
+            key={index}
+            className="flex items-center justify-center md:basis-1/2 lg:basis-1/3"
+          >
+            <div className="p-1">
+              <Card className="">
+                <CardContent className="flex flex-col p-6">
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <p className="text-xl text-pink-500">
+                        {review.Outlet.name}
+                      </p>
+                      <p>{review.alias}</p>
+                    </div>
+                    {review.score && (
+                      <span
+                        className={`inline-flex items-center justify-center ${
+                          review.score >= 80
+                            ? "bg-green-500"
+                            : review.score >= 60
+                            ? "bg-yellow-500"
+                            : review.score >= 40
+                            ? "bg-orange-500"
+                            : "bg-red-500"
+                        } w-10 h-10 rounded-full text-white font-bold`}
+                      >
+                        {review.score}
+                      </span>
+                    )}
+                  </div>
+
+                  <p className="text-xs mt-2">{review.snippet}</p>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 };
 
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,76 +1,81 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import { useEffect, useState } from "react";
-import ReviewCard from "./ReviewCard"
+import ReviewCard from "./ReviewCard";
 
 type ReviewsProps = {
-    title: string | undefined
+  title: string | undefined;
+};
+
+const Reviews: React.FC<ReviewsProps> = ({ title }) => {
+  const [reviewID, setReviewID] = useState(0);
+  const [reviews, setReviews] = useState([]);
+
+  const url = `https://opencritic-api.p.rapidapi.com/game/search?criteria=${title}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "6adda4dea7msh0d84ee148b8b875p14ba25jsn3fd070b06a7c",
+      "X-RapidAPI-Host": "opencritic-api.p.rapidapi.com",
+    },
   };
 
-const Reviews: React.FC<ReviewsProps> = ({title}) => {
-    const [reviewID, setReviewID] = useState(0)
-    const [reviews, setReviews] = useState([])
-
-    const url = `https://opencritic-api.p.rapidapi.com/game/search?criteria=${title}`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '6adda4dea7msh0d84ee148b8b875p14ba25jsn3fd070b06a7c',
-            'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
-        }
-    };
-
-    async function fetchReviewID() {
-        try {
-            const response = await fetch(url, options);
-            const result = await response.json();
-            console.log(result);
-            setReviewID(result[0].id)
-        } catch (error) {
-            console.error(error);
-        }
+  async function fetchReviewID() {
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+      console.log(title);
+      const filteredArr = result.filter(
+        (game: { name: string }) =>
+          game.name.toLowerCase() == title?.toLowerCase()
+      );
+      console.log(filteredArr);
+      if (filteredArr[0]?.id) {
+        setReviewID(filteredArr[0].id);
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    useEffect(() => {
-        fetchReviewID()
-    }, [])
+  useEffect(() => {
+    fetchReviewID();
+  }, []);
 
-    const url2 = `https://opencritic-api.p.rapidapi.com/reviews/game/${reviewID}?skip=20`;
-    const options2 = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '6adda4dea7msh0d84ee148b8b875p14ba25jsn3fd070b06a7c',
-            'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com'
-        }
-    };
+  const url2 = `https://opencritic-api.p.rapidapi.com/reviews/game/${reviewID}?skip=20`;
+  const options2 = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "6adda4dea7msh0d84ee148b8b875p14ba25jsn3fd070b06a7c",
+      "X-RapidAPI-Host": "opencritic-api.p.rapidapi.com",
+    },
+  };
 
-    async function fetchReviews() {
-        try {
-            const response = await fetch(url2, options2);
-            const result = await response.json();
-            console.log(result);
-            setReviews(result)
-        } catch (error) {
-            console.error(error);
-        }
+  async function fetchReviews() {
+    try {
+      const response = await fetch(url2, options2);
+      const result = await response.json();
+      console.log(result);
+      setReviews(result);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    useEffect(() => {
-      fetchReviews()
-    }, [reviewID])
-    
-    
+  useEffect(() => {
+    fetchReviews();
+  }, [reviewID]);
 
   return (
     <div>
-
-        {reviews.length > 1 &&
+      {reviews.length > 1 && (
         <div className="flex justify-center">
-        <ReviewCard reviews={reviews}/>
+          <ReviewCard reviews={reviews} />
         </div>
-        }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
